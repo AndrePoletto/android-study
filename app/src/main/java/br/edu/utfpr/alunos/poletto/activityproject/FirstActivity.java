ddf7ac0122d9b2e391343b5ef164a8994425fdf9
AndrePoletto <polettoandre@gmail.com>
2017-10-02 18:24:53 -0300

    Last commit before exams. =)

--- app/src/main/java/br/edu/utfpr/alunos/poletto/activityproject/FirstActivity.java
+++ app/src/main/java/br/edu/utfpr/alunos/poletto/activityproject/FirstActivity.java
@@ -1,7 +1,9 @@
 package br.edu.utfpr.alunos.poletto.activityproject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -21,6 +23,7 @@ public class FirstActivity extends AppCompatActivity {
     public EditText text_first_name;
     public EditText text_last_name;
     public ConstraintLayout constraintLayout;
+    public int option = Color.RED;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +34,33 @@ public class FirstActivity extends AppCompatActivity {
         text_first_name = (EditText) findViewById(R.id.editText_firstName);
         text_last_name = (EditText) findViewById(R.id.editText_lastName);
         constraintLayout = (ConstraintLayout) findViewById(R.id.mainLayout);
+        readColorPreference();
+    }
+
+    public void readColorPreference(){
+        SharedPreferences sharedPref =
+                getSharedPreferences(getString(R.string.colorPreference),//colorPreference must be on Strings manually
+                        Context.MODE_PRIVATE);
+
+        option = sharedPref.getInt(getString(R.string.bgColor), option);//same as colorPreference
+
+        constraintLayout.setBackgroundColor(option);
+    }
+
+    public void saveColorPreferences(int newValue){
+        SharedPreferences sharedPref =
+                getSharedPreferences(getString(R.string.colorPreference),
+                        Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putInt(getString(R.string.bgColor), newValue);
+
+        editor.commit();
+
+        option = newValue;
+
+        constraintLayout.setBackgroundColor(option);
     }
 
     public void btnShowClicked(View view) {
@@ -79,21 +109,40 @@ public class FirstActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        switch(option){
+            case Color.RED:
+                menu.getItem(0).setChecked(true);
+                return true;
+            case Color.GREEN:
+                menu.getItem(1).setChecked(true);
+                return true;
+            case Color.BLUE:
+                menu.getItem(2).setChecked(true);
+                return true;
+            default:
+                return false;
+
+        }
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         item.setChecked(true);
 
         switch (item.getItemId()) {
 
             case R.id.redItem:
-                constraintLayout.setBackgroundColor(Color.RED);
+                saveColorPreferences(Color.RED);
                 return true;
 
             case R.id.greenItem:
-                constraintLayout.setBackgroundColor(Color.GREEN);
+                saveColorPreferences(Color.GREEN);
                 return true;
 
             case R.id.blueItem:
-                constraintLayout.setBackgroundColor(Color.BLUE);
+                saveColorPreferences(Color.BLUE);
                 return true;
 
             default:
